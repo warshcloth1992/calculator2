b9bdfc732a70c67985ab189879760c1edcab5dfa
Your Name <you@example.com>
2020-01-29 13:07:32 -0800

    Updated with addition wip

--- calculator.py
+++ calculator.py
@@ -6,5 +6,12 @@ calculator program yourself in this file.
 
 from arithmetic import *
 
+input_string = ""
 
-# Your code goes here
+while input_string != "q":
+    input_string = input("enter tokens here: ")
+    TokensList = input_string.split(" ")
+
+    if TokensList[0] == "+":
+
+        print(add(int(TokensList[1]), int(TokensList[2])))
